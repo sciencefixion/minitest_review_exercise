b55defdde8188ff7fa50fe30d9bc94b476fbc7ac
Rion Hutsell <st.gish+gh@pm.me>
2021-05-17 21:23:14 -0600

    Add magic_ball_spec.rb

---
+++ magic_ball_spec.rb
@@ -0,0 +1,28 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "minitest/spec"
+require_relative "magic_ball"
+
+describe MagicBall do
+  describe "#ask" do
+    it "returns an answer" do
+      magic_ball = MagicBall.new
+      assert_includes MagicBall::ANSWERS, magic_ball.ask("Is 42 the answer?")
+    end
+
+    it "predefined answers is array" do
+      assert_kind_of Array, MagicBall::ANSWERS
+    end
+
+    it "predefined answers is not empty" do
+      refute_empty MagicBall::ANSWERS
+    end
+
+    it "raises error when question is number" do
+      assert_raises "Question has invalid format." do
+        magic_ball = MagicBall.new
+        magic_ball.ask(1)
+      end
+    end
+  end
+end
